9aa7694a8acbaf6e9b0291df0aa5287781e63a06
devarsh.s <devarsh.s@latitudetechnolabs.com>
2023-03-23 18:29:21 +0530

    cleaned - home-create-home

--- developers_portfolio/hire_talents/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Question,Option
-
-class OptionForm(forms.ModelForm):
-    class Meta:
-        model = Option
-        fields = '__all__'
--- developers_portfolio/hire_talents/views.py
+++ developers_portfolio/hire_talents/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
-from .forms import OptionForm
-from .models import Question,Option
 
 from djf_surveys.views import CreateSurveyFormView
 from django.contrib import messages
@@ -11,22 +9,20 @@ from django.utils.translation import gettext, gettext_lazy as _
 # Create your views here.
 
 class CreateSurveyFormViewUpdated(CreateSurveyFormView):
-    success_url = reverse_lazy("index")
-    def __init__(self):
-        print("###INSIDE")
+    success_url = reverse_lazy("home")
 
     def dispatch(self, request, *args, **kwargs):
         survey = self.get_object()
         # handle if survey can_anonymous_user
         if not request.user.is_authenticated and not survey.can_anonymous_user:
             messages.warning(request, gettext("Sorry, you must be logged in to fill out the survey."))
-            return redirect("index")
+            return redirect("home")
 
         # handle if user have answer survey
         if request.user.is_authenticated and not survey.duplicate_entry and \
                 UserAnswer.objects.filter(survey=survey, user=request.user).exists():
             messages.warning(request, gettext("You have submitted out this survey."))
-            return redirect("index")
+            return redirect("home")
         return super(CreateSurveyFormView,self).dispatch(request, *args, **kwargs)
     
 
