1e340c118a44e4dfe6db7e7ce6279673f626301e
devarsh.s <devarsh.s@latitudetechnolabs.com>
2023-04-13 17:55:26 +0530

    login required urls and links added

--- developers_portfolio/developers_portfolio/urls.py
+++ developers_portfolio/developers_portfolio/urls.py
@@ -4,21 +4,28 @@ from hire_talents import views as hire_views
 from djf_surveys import views 
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth.decorators import login_required
 
 from djf_surveys.app_settings import SURVEYS_ADMIN_BASE_PATH 
 
 
 urlpatterns = [
+
+    #Requires LOGIN
     path('admin/', admin.site.urls),
+    path('company',login_required(hire_views.company),name='company'),
+    path('inquiries',login_required(hire_views.inquiries),name='inquiries'),
+    path('detail/<int:pk>/',login_required(hire_views.detail),name='detail'),
+    path('completed_meeting/<int:pk>/',login_required(hire_views.completed_meeting),name='completed_meeting'),
+    # path('surveys/create/create-employee/', login_required(hire_views.CreateSurveyFormView.as_view()), name='create'),    
+    path('surveys/', include('djf_surveys.urls')),
+
+    #Doesnot Require LOGIN
     path('',hire_views.home,name='home'),
     path('display',hire_views.display,name='display'),
     path('why',hire_views.why,name='why'),
-    path('inquiries',hire_views.inquiries,name='inquiries'),
-    path('detail/<int:pk>/',hire_views.detail,name='detail'),
-    path('completed_meeting/<int:pk>/',hire_views.completed_meeting,name='completed_meeting'),
     path('searching',hire_views.searching,name='searching'),
     path('appointment_page',hire_views.appointment_page,name='appointment_page'),
     path('surveys/create/<str:slug>/', hire_views.CreateSurveyFormView.as_view(), name='create'),    
-    path('surveys/', include('djf_surveys.urls')),
 
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
